e571d55a9f54ddb1545aac77038cee2a28b124e7
aryanshukla135 <aryanshukla135@gmail.com>
2026-06-27 15:02:50 +0530

    Time: 351 ms (67.94%), Space: 103.1 MB (65.88%) - LeetHub

---
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        bool flip = true ;
+        int delrow[] = {+1,0,-1,0};
+        int delcol[] = {0,+1,0,-1};
+        vector<vector<int>> dist(n , vector<int>(m,INT_MAX));
+        //{time,{parirty,{row,col}}}
+       priority_queue<pair<int,pair<int,pair<int,int>>>,vector<pair<int,pair<int,pair<int,int>>>>,greater<pair<int,pair<int,pair<int,int>>>>> pq ;
+
+        pq.push({0,{0,{0,0}}});
+        dist[0][0] = 0 ;
+
+        while(!pq.empty()){
+            int time = pq.top().first;
+            int row = pq.top().second.second.first ;
+            int col = pq.top().second.second.second;
+            int parity = pq.top().second.first;
+
+            pq.pop();
+
+            for(int i =0; i< 4 ; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+              
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    int val = 1 ;
+                    if(parity == 1){
+                        val =2;
+                    }
+                    int ntime = max(moveTime[nrow][ncol],time) +val;
+                    if(ntime <dist[nrow][ncol]){
+                        dist[nrow][ncol] =ntime;
+                        pq.push({dist[nrow][ncol],{!parity,{nrow,ncol}}});
+            
+                    }
+                }
+            }
+
+        }
+        return dist[n-1][m-1];
+    }
+};
