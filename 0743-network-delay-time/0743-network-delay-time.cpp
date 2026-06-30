3535eeea7c9258ea100ccd2f0fb268bc474540d9
aryanshukla135 <aryanshukla135@gmail.com>
2026-06-30 14:16:28 +0530

    Time: 102 ms (10.3%), Space: 48.1 MB (9.58%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int,list<pair<int,int>>> adj;
-        for(int i = 0 ; i< times.size();i++){
-            int u =times[i][0];
-            int v = times[i][1];
-            int w = times[i][2];
-            pair<int,int> p = make_pair(v,w);
-            adj[u].push_back(p);
+        vector<vector<pair<int,int>>> graph(n+1);
+        for(auto it : times){
+            graph[it[0]].push_back({it[1],it[2]});
         }
-        vector<int> dist(n+1,INT_MAX);
-        set<pair<int,int>>st;
-        dist[k]=0;
-        st.insert({0,k});
 
-        while(!st.empty()){
-            auto top = *(st.begin());
-            st.erase(st.begin());
-            int node=top.second;
-            int d = top.first ;
+        priority_queue<pair<int,int>,vector<pair<int,int>> , greater<pair<int,int>>> pq ;
+        vector<int> dist(n+1 , INT_MAX);
 
-            for(auto i:adj[node]){
-                int a = i.first;
-                int b = i.second;
+        pq.push({0,k});
+        dist[k] = 0 ;
 
-                if(d+b<dist[a]){
-                     if(dist[a] !=INT_MAX){
-                        st.erase({dist[a],a});
-                     }
-                     dist[a]=d+b;
-                     st.insert({dist[a],a});
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+
+            int distance = it.first ;
+            int node = it.second ;
+
+            for(auto i: graph[node]){
+                int adjnode = i.first ;
+                int edgedist = i.second ;
+
+                if(edgedist + distance < dist[adjnode]){
+                    dist[adjnode] = edgedist + distance;
+                    pq.push({dist[adjnode] , adjnode});
                 }
             }
         }
-        int ans =0;
-        for(int i= 1;i<=n;i++){
-            if(dist[i]==INT_MAX){
-                return -1;
-            }
-            ans=max(ans,dist[i]);
+        dist[0] = 0;
+        int maxi = *max_element(dist.begin() , dist.end());
+        if(maxi == INT_MAX){
+            return -1;
         }
-     return ans ;
-        
+        return maxi;
     }
 };
