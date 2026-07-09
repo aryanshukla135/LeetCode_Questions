49d6483b99f599a4c72344f0b0b4cb0f41273384
ARYAN SHUKLA <aryanshukla135@gmail.com>
2026-07-09 09:32:12 +0530

    Time: 0 ms (100%), Space: 11 MB (73.11%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> st ;
+        for(int i =0 ; i< n ; i++){
+            if(nums[i] != 0)  st.insert(nums[i]);
+    
+        }
+        return st.size();
+    }
+};
+// 1 2 3 4 
