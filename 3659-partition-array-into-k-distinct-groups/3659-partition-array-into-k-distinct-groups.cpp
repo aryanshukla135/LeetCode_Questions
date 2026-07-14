316d6d86ebe8e940274fb2cd91eff5783aed1a0a
ARYAN SHUKLA <aryanshukla135@gmail.com>
2026-07-14 12:57:28 +0530

    Time: 51 ms (58.86%), Space: 285.8 MB (57.59%) - LeetHub

---
+++ 3659-partition-array-into-k-distinct-groups/3659-partition-array-into-k-distinct-groups.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool partitionArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(k>n || n % k != 0){
+            return false ;
+        }
+
+        int part = n / k ;
+
+        unordered_map<int,int> mp ;
+        for(int i =0 ; i<n ;i++){
+            mp[nums[i]]++;
+        }
+
+        for(int i =0 ; i<n ; i++){
+            if(mp[nums[i]] > part){
+                return false ;
+            }
+        }
+
+        return true;
+    }
+};
+// 1 2 3 5 4 4  , k = 3
