1fb3f54f7ef88bc78b24b36b7cfd86fdfef0a45f
ARYAN SHUKLA <aryanshukla135@gmail.com>
2026-08-24 22:20:05 +0530

    Time: 51 ms (25.78%), Space: 48.8 MB (42.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -21,8 +21,32 @@ public:
     int coinChange(vector<int>& coins, int amount) {
          int n = coins.size();
          vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-         int ans = f(coins,n-1,amount,dp);
+         // momorization 
+        // int ans = f(coins,n-1,amount,dp);
+
+         // taboulation 
+         for(int t = 0 ; t <= amount ; t++){
+            if(t % coins[0] == 0){
+                dp[0][t] = t / coins[0];
+            }else{
+                dp[0][t] = 1e9;
+            }
+         }
+
+         for(int i = 1 ; i<n ; i++){
+            for(int t =0 ; t <= amount; t++){
+                int nottake = 0 + dp[i-1][t];
+                int take = 1e9 ;
+
+                if(t >= coins[i]){
+                    take = 1 + dp[i][t - coins[i]];
+                }
+                dp[i][t] =  min(take,nottake);
+            }
+         }
+         int ans = dp[n-1][amount];
          if(ans == 1e9) return -1;
+
          return ans ;
     }
 };
