daf14e3a7a357dc106e0ab2af2836a5df454909c
ARYAN SHUKLA <aryanshukla135@gmail.com>
2026-09-12 11:09:04 +0530

    Time: 6 ms (57.58%), Space: 12.4 MB (86.66%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -8,22 +8,35 @@ class Solution {
         int ans =0;
         for(int i =0 ; i<k ; i++ ){
             int index = ind + i ;
-           // cout<<index<<endl;
             if(index<nums.size()){
-              maxi = max(maxi,nums[index]);
-    
+                maxi = max(maxi,nums[index]);
                 int temp = maxi*(i+1);
                 ans = max(ans,temp + solve(nums,index+1,k,dp));
-            }
-            
+            } 
         }
         return dp[ind] = ans ;
     }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,0);
         int ans = solve(arr,0,k,dp);
-        return ans ;
+
+        //// tabulation 
+        for(int ind = n ; ind>=0 ; ind--){
+            int maxi = 0;
+            int ans = 0;
+            for(int i = 0 ; i<k ; i++){
+                int index = ind + i ;
+                if(index<arr.size()){
+                    maxi = max(maxi,arr[index]);
+                    int temp = maxi*(i+1);
+                    ans = max(ans,temp + dp[index+1]);
+                } 
+            }
+            dp[ind] = ans ;
+        }
+
+        return dp[0] ;
     }
 };
